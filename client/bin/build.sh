eacc4787a189a9e0d7a348902af15b39ffbd7fcc
Joonas Muhonen <jtmuhone@gmail.com>
2012-03-09 15:15:41 +0200

    Improved build/test scripts.

--- client/bin/build.sh
+++ client/bin/build.sh
@@ -5,12 +5,19 @@ JSBB_CLIENT_HOME="$DIR/.."
 JSBB_BUILD_DIR="$JSBB_CLIENT_HOME/build"
 
 cd $JSBB_CLIENT_HOME
-./js jsbb/scripts/build.js
+java -Xmx512m -Xss1024k \
+    -cp steal/rhino/js.jar \
+    org.mozilla.javascript.tools.shell.Main \
+    -opt -1 \
+    -e 'load('"'"jsbb/scripts/build.js"'"')'
+EXITCODE=$?
 
-mkdir -p $JSBB_BUILD_DIR/jsbb
-mv -f $JSBB_CLIENT_HOME/jsbb/production.js $JSBB_BUILD_DIR/jsbb
-mv -f $JSBB_CLIENT_HOME/jsbb/production.css $JSBB_BUILD_DIR/jsbb 
-cp -f $JSBB_CLIENT_HOME/jsbb/index.html $JSBB_BUILD_DIR/jsbb
+if [ $EXITCODE -eq 0 ]; then
+    mkdir -p $JSBB_BUILD_DIR/jsbb
+    mv -f $JSBB_CLIENT_HOME/jsbb/production.js $JSBB_BUILD_DIR/jsbb
+    mv -f $JSBB_CLIENT_HOME/jsbb/production.css $JSBB_BUILD_DIR/jsbb 
+    cp -f $JSBB_CLIENT_HOME/jsbb/index.html $JSBB_BUILD_DIR/jsbb
 
-mkdir -p $JSBB_BUILD_DIR/steal
-cp -f $JSBB_CLIENT_HOME/steal/steal.production.js $JSBB_BUILD_DIR/steal
+    mkdir -p $JSBB_BUILD_DIR/steal
+    cp -f $JSBB_CLIENT_HOME/steal/steal.production.js $JSBB_BUILD_DIR/steal
+fi
--- client/bin/func-test.sh
+++ client/bin/func-test.sh
@@ -4,4 +4,10 @@ DIR=`dirname $0`
 JSBB_CLIENT_HOME="$DIR/.."
 
 cd $JSBB_CLIENT_HOME
-./js funcunit/run phantomjs jsbb/funcunit.html
+java -Xmx512m -Xss1024k \
+    -cp steal/rhino/js.jar \
+    org.mozilla.javascript.tools.shell.Main \
+    -e "_args=['phantomjs','jsbb/funcunit.html']" \
+    -opt -1 \
+    -e "load('funcunit/run')"
+EXITCODE=$?
